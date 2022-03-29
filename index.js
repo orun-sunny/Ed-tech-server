360c18f2439c6656c9281771057ca1eff0c25748
orun-sunny <sunny.mukut@gmail.com>
2022-03-29 11:58:32 +0600

    added some

---
+++ index.js
@@ -0,0 +1,221 @@
+
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const ObjectId = require("mongodb").ObjectId;
+const port = process.env.PORT || 5000;
+const app = express();
+
+//MIDDLEWARE added
+app.use(cors());
+app.use(express.json());
+
+// API added
+app.get("/", async (req, res) => {
+  res.send("server is cholse!");
+});
+
+//URI
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmd33.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+
+
+
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmd33.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmd33.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//DB CONNECTION added 
+//env file added extra
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Ed-tech");
+    const user_collection = database.collection("users");
+    const product_collection = database.collection("products");
+    const order_collection = database.collection("orders");
+    const review_collection = database.collection("review");
+
+    // post api added
+    app.post("/users", async (req, res) => {
+      const result = await user_collection.insertOne(req.body);
+      res.json(result);
+    });
+
+    // post api added here
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await user_collection.findOne({ email: email });
+      res.json(result);
+    });
+
+    // post api added here
+    app.put("/addAdmin", async (req, res) => {
+      const email = req.body.email;
+      const result = await user_collection.updateOne(
+        { email },
+        {
+          $set: { role: "admin" },
+        }
+      );
+      res.json(result);
+    });
+
+    //load: get api added here
+    app.get("/products", async (req, res) => {
+      const result = await product_collection.find({}).toArray();
+      res.json(result);
+    });
+
+
+    // post api added here
+    app.post("/placeorder", async (req, res) => {
+      const order = req.body;
+      order.status = "Pending";
+      delete order._id;
+      const result = await order_collection.insertOne(order);
+      res.json(result);
+    });
+
+    // load: get api added here
+    app.get("/placeorder/:id", async (req, res) => {
+      const result = await product_collection.findOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.json(result);
+    });
+
+
+
+
+    //- get all  api here
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      let result;
+      if (email) {
+        result = await order_collection.find({ email }).toArray();
+      } else {
+        result = await order_collection.find({}).toArray();
+      }
+      res.json(result);
+    });
+
+    //put api added here
+    app.put("/updateOrderStatus", async (req, res) => {
+      const id = req.body.id;
+      const status = req.body.status;
+      const result = await order_collection.updateOne(
+        { _id: ObjectId(id) },
+        {
+          $set: { status: status },
+        }
+      );
+      res.json(result.modifiedCount);
+    });
+
+    //update product here
+    app.put("/updateProduct", async (req, res) => {
+      const id = req.query.id;
+      const product = req.body;
+      const result = await product_collection.updateOne(
+        { _id: ObjectId(id) },
+        {
+          $set: product,
+        }
+      );
+      res.json(result);
+    });
+
+
+    app.delete("/placeorder/:id", async (req, res) => {
+      const result = await order_collection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.json(result);
+    });
+    //added here post of add review
+    app.post("/addReview", async (req, res) => {
+      const result = await review_collection.insertOne(req.body);
+      res.json(result);
+    });
+
+    // add product here
+    app.post("/addProduct", async (req, res) => {
+      const result = await product_collection.insertOne(req.body);
+      res.json(result);
+    });
+
+
+    //add review here
+
+    app.get("/reviews", async (req, res) => {
+      const result = await review_collection.find({}).toArray();
+      res.json(result);
+    });
+
+    // delete product here
+    app.delete("/deleteProduct/:id", async (req, res) => {
+      const result = await product_collection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.json(result);
+    });
+
+
+    app.get("/updateOne/:id", async (req, res) => {
+      const result = await product_collection.findOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.json(result);
+    });
+    // payment main part
+//    ---------------Payment Section started---------------
+// define payment data get for stripe payments
+app.post('/create-payment-intent', async (req, res) => {
+  const paymentInfo = req.body;
+  const paymentAmount = parseInt(paymentInfo?.cost)
+  const amount = paymentAmount * 100;
+  const paymentIntent = await stripe.paymentIntents.create({
+      currency: 'usd',
+      amount: amount,
+      payment_method_types: ['card']
+  });
+  res.json({ clientSecret: paymentIntent.client_secret })
+});
+
+//---put api
+app.put('/orders/:id', async (req, res) => {
+  const id = req.params.id;
+  const payment = req.body;
+  const filter = { _id: ObjectId(id) };
+  const updateDoc = {
+      $set: {
+          payment: payment
+      }
+  }
+  const result = await order_collection.updateOne(filter, updateDoc);
+  res.json(result)
+});
+app.get('/orders/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await order_collection.findOne(query);
+  res.send(result);
+  // ai part er pore data ta /5000/users/id te pabo 
+});
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
